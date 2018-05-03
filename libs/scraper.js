c72fe2fd27f954f752bbcdc38a847dc3ebbb6a42
Giuseppe Verni <g.verni@gmail.com>
2018-05-03 19:14:44 +0100

    test

--- libs/scraper.js
+++ libs/scraper.js
@@ -3,7 +3,7 @@ const cheerio = require('cheerio')
 const debug = require('debug')('scraper')
 
 function scrape (url, fnSectionSelector, itemsSelectors) {
-  return fetch(url, {follow: 10})
+  return fetch(url)
     .then(response => response.text())
     .then(responseHtml => {
       const $ = cheerio.load(responseHtml)
