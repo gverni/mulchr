6afeac36fd2164eb15290febb926006b27d35251
Giuseppe Verni <g.verni@gmail.com>
2018-05-12 19:06:38 +0100

    Fixed bugs and added cache test

--- test/rss.js
+++ test/rss.js
@@ -1,18 +1,25 @@
 var chai = require('chai')
 var app = require('../app')
 const fetch = require('node-fetch')
+const sinon = require('sinon')
 
 var expect = chai.expect
 
-describe('RSS', function () {
+describe('Testing RSS services', function () {
   var server
-  this.timeout(5000)
-  console.log("Are we on travis?" + process.env.TRAVIS)
+  this.timeout(10000)
 
   before(() => {
+    // Remove cache
+    // Spyting the debug library
+    sinon.spy(process.stderr, 'write')
     server = app.listen(3000)
   })
-  after(() => server.close())
+  after(() => {
+    // Restore debug library
+    process.stderr.write.restore()
+    server.close()
+  })
 
   it('Amazon Italia Kindle Offerta Lampo', function (done) {
     fetch('http://127.0.0.1:3000/am-it-kindle-offerta-lampo')
@@ -47,6 +54,8 @@ describe('RSS', function () {
         return res.text()
       }).then(text => {
         if (!process.env.TRAVIS) {
+          // Travis run from a non-UK server, so fetching Audible UK page
+          // causes too many redirect. So we skip the test on travis
           expect(text).to.contain('<item>')
           expect(text).to.contain('</item>')
         }
@@ -55,4 +64,25 @@ describe('RSS', function () {
       .catch((err) => { done(err) })
   })
 
+  it('Check usage of cache', function (done) {
+    this.timeout(30000)
+    fetch('http://127.0.0.1:3000/am-it-kindle-offerta-lampo')
+      .then(response => response.text())
+      .then(() => fetch('http://127.0.0.1:3000/am-uk-kindle-daily-deal'))
+      .then(response => response.text())
+      .then(() => fetch('http://127.0.0.1:3000/au-uk-daily-deal'))
+      .then(response => response.text())
+      .then(() => {
+        let callsNo = 0 
+        process.stderr.write.getCalls().forEach((spyCall) => {
+          if (/.*?Using cache.*?/.test(spyCall.args[0])) { callsNo++ }
+        })
+        if ((process.env.TRAVIS && callsNo === 2) ||
+              (!process.env.TRAVIS && callsNo === 3)) {
+          done()
+        } else {
+          done('\'Using cache\' called ' + callsNo + ' times')
+        }
+      })
+  })
 })
