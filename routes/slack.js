1744c87269ac74b5eed67b85b51a9b4058068c78
Giuseppe Verni <g.verni@gmail.com>
2018-06-13 18:15:03 +0100

    Dynamic handling of all the services

--- routes/slack.js
+++ routes/slack.js
@@ -1,9 +1,12 @@
 var express = require('express')
 var router = express.Router()
 
-function slackify (title, data) {
+var availableServices = ['am-it-kindle-offerta-lampo', 'am-uk-kindle-daily-deal', 'au-uk-daily-deal' ]
+var availableServicesTitle = ['Amazon Italia Kindle Offerta Lampo', 'Amazon UK Kindle Daily Deal', 'Audible UK Daily deal']
+
+function slackify (service, data) {
   var tmpObj = {}
-  tmpObj = {'text': title}
+  tmpObj = {'text': availableServicesTitle[availableServices.indexOf(service)]}
   tmpObj.attachments = []
   data.forEach(element => {
     tmpObj.attachments.push({'text': element.title})
@@ -12,20 +15,21 @@ function slackify (title, data) {
 }
 
 router.get('/', function (req, res, next) {
-  res.send('Placeholder for slack app installation')
+  res.setHeader('Content-Type', 'text/plain')
+  res.send('Placeholder for slack app installation\r\n' + JSON.stringify(slackify('am-it-kindle-offerta-lampo', req.app.locals.cachedb['am-it-kindle-offerta-lampo'])))
 })
 
 router.post('/', function (req, res, next) {
   var service = req.body.text
   res.setHeader('Content-Type', 'application/json')
   if (service) {
-    res.send(slackify(service, req.app.locals.cachedb['am-it-kindle-offerta-lampo']))
+    res.send(slackify(service, req.app.locals.cachedb[service]))
   } else {
     res.send({
       'text': 'Mulchr: List of available services:',
       'attachments': [
         {
-          'text': 'uk-kindle-daily-deal\nit-kindle-daily-deal\nuk-audible'
+          'text': availableServices.join('\n')
         }
       ]
     })
