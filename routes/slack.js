f96043776240554ff7cc74127e69ba1c5ddce196
Giuseppe Verni <g.verni@gmail.com>
2018-06-13 15:45:01 +0100

    Added handling of text field from slack

--- routes/slack.js
+++ routes/slack.js
@@ -6,15 +6,20 @@ router.get('/', function (req, res, next) {
 })
 
 router.post('/', function (req, res, next) {
+  var service = req.body.text
   res.setHeader('Content-Type', 'application/json')
-  res.send({
-    'text': 'Mulchr: List of available services:',
-    'attachments': [
-      {
-        'text': 'uk-kindle-daily-deal\nit-kindle-daily-deal\nuk-audible'
-      }
-    ]
-  })
+  if (service) {
+    res.send({'text': 'Mulching ' + service + '...'})
+  } else {
+    res.send({
+      'text': 'Mulchr: List of available services:',
+      'attachments': [
+        {
+          'text': 'uk-kindle-daily-deal\nit-kindle-daily-deal\nuk-audible'
+        }
+      ]
+    })
+  }
 })
 
 module.exports = router
