8a8dda755694d1895f8598ce5566d48f83ee9d3f
Giuseppe Verni <g.verni@gmail.com>
2018-06-13 15:58:58 +0100

    Added handling of dynamic content

--- routes/slack.js
+++ routes/slack.js
@@ -1,6 +1,16 @@
 var express = require('express')
 var router = express.Router()
 
+function slackify (title, data) {
+  var tmpObj = {}
+  tmpObj = {'text': title}
+  tmpObj.attachments = []
+  data.forEach(element => {
+    tmpObj.attachments.push({'text': element.title})
+  })
+  return tmpObj
+}
+
 router.get('/', function (req, res, next) {
   res.send('Placeholder for slack app installation')
 })
@@ -9,7 +19,7 @@ router.post('/', function (req, res, next) {
   var service = req.body.text
   res.setHeader('Content-Type', 'application/json')
   if (service) {
-    res.send({'text': 'Mulching ' + service + '...'})
+    res.send(slackify(service, req.app.locals.cachedb['am-it-kindle-offerta-lampo']))
   } else {
     res.send({
       'text': 'Mulchr: List of available services:',
