b96f0e869a4ec7e5e2bb90292ab52886dbb9befa
Giuseppe Verni <g.verni@gmail.com>
2018-07-16 22:19:36 +0100

    systemv installtion files

---
+++ install/linux/install.sh
@@ -0,0 +1,4 @@
+# Customize mulchr.service specifying execution and working folder
+sudo cp mulchr.service /etc/systemd/system/         
+sudo systemctl enable mulchr.service              
+sudo systemctl start mulchr.service      
