241779bf10ea3fd674170e04114d5985c6dc7e32
Giuseppe Verni <g.verni@gmail.com>
2018-08-03 16:30:47 +0100

    Added slack request validation

--- routes/slack.js
+++ routes/slack.js
@@ -14,8 +14,22 @@ function slackify (service, data) {
   return tmpObj
 }
 
+function validateSlackRequest(httpReq) {
+  const SlackAppSigningSecret = process.env.MLUCHR_SLACK_SIGNING_SECRET
+  var xSlackRequestTimeStamp = httpReq.get('X-Slack-Request-Timestamp')
+  var baseString = 'v0:' + xSlackRequestTimeStamp + ':' + req.body.payload
+  console.log('Slack validation base string: ' + baseString)
+  const hash = 'v0=' + crypto.createHmac('sha256', SlackAppSigningSecret)
+    .update(baseString)
+    .digest('hex')
+  
+  console.log('hash')
+  console.log(httpReq.get('X-Slack-Signature'))
+  return (httpReq.get('X-Slack-Signature') === hash)
+}
+
 router.get('/', function (req, res, next) {
-  // Use blow for debugging
+  // Use below for debugging
   // var availableServicesMessage = {}
   // availableServicesMessage['text'] = 'Let\'s get mulching...'
   // availableServicesMessage['attachments'] = [{'text': 'Choose the service:', 'attachment_type': 'default', 'actions': []}]
@@ -31,17 +45,22 @@ router.get('/', function (req, res, next) {
 
 router.post('/', function (req, res, next) {
   var service = (req.body.payload) ? JSON.parse(req.body.payload).actions[0].value : req.body.text
-  res.setHeader('Content-Type', 'application/json')
-  if (service) {
-    res.send(slackify(service, req.app.locals.cachedb[service]))
+  if (validateSlackRequest(req)) {
+      res.setHeader('Content-Type', 'application/json')
+    if (service) {
+      res.send(slackify(service, req.app.locals.cachedb[service]))
+    } else {
+      var availableServicesMessage = {}
+      availableServicesMessage['text'] = 'Let\'s get mulching...'
+      availableServicesMessage['attachments'] = [{'text': 'Choose the service:', 'callback_id': 'service', 'color': '#3AA3E3', 'attachment_type': 'default', 'actions': []}]
+      availableServices.forEach((service, index) => {
+        availableServicesMessage['attachments'][0]['actions'].push({'name': service, 'text': availableServicesTitle[index], 'type': 'button', 'value': service})
+      })
+      res.send(availableServicesMessage)
+    }
   } else {
-    var availableServicesMessage = {}
-    availableServicesMessage['text'] = 'Let\'s get mulching...'
-    availableServicesMessage['attachments'] = [{'text': 'Choose the service:', 'callback_id': 'service', 'color': '#3AA3E3', 'attachment_type': 'default', 'actions': []}]
-    availableServices.forEach((service, index) => {
-      availableServicesMessage['attachments'][0]['actions'].push({'name': service, 'text': availableServicesTitle[index], 'type': 'button', 'value': service})
-    })
-    res.send(availableServicesMessage)
+    // If it's not a slack request we send 403 error
+    res.sendStatus(403)
   }
 })
 
